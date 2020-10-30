2ca0aa40eacbc6fe96adc3c92d3d579532c3f0de
YeydelF <56987545+YeydelF@users.noreply.github.com>
2020-10-29 21:04:26 -0600

    Agregar clase buscar

---
+++ ing_software_PCCV/Controlador/Buscar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+using Datos;
+namespace Controlador
+{
+    public class Buscar
+    {
+       public void Filtrar(DataGridView data, string busqueda)
+        {
+            Conexion con = new Conexion();
+            try
+            {
+                SqlCommand sql = new SqlCommand("SPFiltroBusqueda",con.AbrirConexion());
+                sql.CommandType = CommandType.StoredProcedure;
+                sql.Parameters.Add("@Filtro", SqlDbType.NVarChar, 100).Value = busqueda;
+
+                sql.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(sql);
+                da.Fill(dt);
+                data.DataSource = dt;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                con.CerrarConexion();
+            }
+        }
+    }
+}
