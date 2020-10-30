4b793e8b79c89cafff8cdcc638e233bc566bbc9c
AaronCanales115 <aaroncoresykes50@gmail.com>
2020-10-29 22:53:12 -0600

    cambios

--- ing_software_PCCV/Datos/DtProductos.cs
+++ ing_software_PCCV/Datos/DtProductos.cs
@@ -39,8 +39,8 @@ namespace Datos
             comando.Parameters.AddWithValue("@Stock", stock);
             comando.Parameters.AddWithValue("@Talla", talla);
             comando.Parameters.AddWithValue("@Categoria", categoria);
-            comando.Parameters.AddWithValue("@Estado", stock);
-            comando.Parameters.AddWithValue("@Usuario", talla);
+            comando.Parameters.AddWithValue("@Estado", estado);
+            comando.Parameters.AddWithValue("@Usuario", usuario);
             comando.Parameters.AddWithValue("@Resultado", resultado);
             comando.ExecuteNonQuery();
             comando.Parameters.Clear();
