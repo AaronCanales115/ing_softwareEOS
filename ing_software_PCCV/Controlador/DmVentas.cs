e4855e18ff3da79fe365468c8d6d91ca51883c6a
AaronCanales115 <aaroncoresykes50@gmail.com>
2020-11-11 20:24:42 -0600

    agregar clase de ventas

---
+++ ing_software_PCCV/Controlador/DmVentas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+
+namespace Controlador
+{
+    class DmVentas
+    {
+        private DtVentas oDt = new DtVentas();
+
+        public void agregarVenta(string NFactura, string usuario, string nombreC, string apellidoC, string cantidadProductos, string descuento, string iva, string subTotal, string total)
+        {
+            oDt.agregarVenta(NFactura, Convert.ToInt16(usuario), nombreC, apellidoC, Convert.ToInt16(cantidadProductos), Convert.ToDecimal(descuento), Convert.ToDecimal(iva), Convert.ToDecimal(subTotal), Convert.ToDecimal(total));
+        }
+
+        public void ventaProducto(string idProducto, string idv, string cantidad)
+        {
+            oDt.ventaProducto(Convert.ToInt16(idProducto), Convert.ToInt16(idv), Convert.ToInt16(cantidad));
+        }
+    }
+}
---
+++ ing_software_PCCV/Datos/DtVentas.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Datos
+{
+    public class DtVentas
+    {
+        private Conexion conexion = new Conexion();
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+
+        public string agregarVenta(string NFactura, int usuario, string nombreC, string apellidoC, int cantidadProductos, decimal descuento, decimal iva, decimal subTotal, decimal total)
+        {
+            try
+            {
+                comando.Connection = conexion.AbrirConexion();
+                comando.CommandText = "SPAgregarVenta";
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@NFactura", NFactura);
+                comando.Parameters.AddWithValue("@Usuario", usuario);
+                comando.Parameters.AddWithValue("@NombreC", nombreC);
+                comando.Parameters.AddWithValue("@ApellidoC", apellidoC);
+                comando.Parameters.AddWithValue("@cantidadProductos", cantidadProductos);
+                comando.Parameters.AddWithValue("@descuento", descuento);
+                comando.Parameters.AddWithValue("@iva", iva);
+                comando.Parameters.AddWithValue("@subTotal", subTotal);
+                comando.Parameters.AddWithValue("@Total", total);
+                comando.ExecuteNonQuery();
+                comando.Parameters.Clear();
+                return "OK";
+            }
+            catch (Exception)
+            {
+                return "NO";
+            }
+        }
+
+        public string ventaProducto(int idProducto, int idv, int cantidad)
+        {
+            try
+            {
+                comando.Connection = conexion.AbrirConexion();
+                comando.CommandText = "SPVentaProducto";
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@idProducto", idProducto);
+                comando.Parameters.AddWithValue("@idv", idv);
+                comando.Parameters.AddWithValue("@Cantidad", cantidad);
+                comando.ExecuteNonQuery();
+                comando.Parameters.Clear();
+                return "OK";
+            }
+            catch (Exception)
+            {
+                return "NO";
+            }
+        }
+
+    }
+}
